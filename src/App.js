5a6558715bd8d10648571054b92334863b0724f9
Teo Marcelo <tmarcelojr@gmail.com>
2020-12-14 10:59:15 -0600

    register route trial

--- src/App.js
+++ src/App.js
@@ -19,8 +19,9 @@ function App() {
       body: JSON.stringify(user),
       headers: { "Content-Type": "application/json" }
     })
-      .then(res => res.json())
-      .then(data => console.log(data))
+      .then(res => console.log(res))
+      // .then(res => res.json())
+      // .then(data => console.log(data))
   }
 
   const login = () => {
